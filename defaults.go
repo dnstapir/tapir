d4613c756c1abeab124b52c69d765ec0f6c975cf
Johan Stenstam <johani@johani.org>
2024-02-16 14:31:23 +0100

    foo

--- defaults.go
+++ defaults.go
@@ -7,6 +7,7 @@ import ()
 
 const DefaultTemCfgFile = "/etc/dnstapir/tem.yaml"
 const TemSourcesCfgFile = "/etc/dnstapir/tem-sources.yaml"
+const TemOutputsCfgFile = "/etc/dnstapir/tem-outputs.yaml"
 const TemPolicyCfgFile = "/etc/dnstapir/tem-policy.yaml"
 
 const DefaultTapirCliCfgFile = "/etc/dnstapir/tapir-cli.yaml"
